ac9f4a61b37f461ea9f1f65a62ebab6df18ffbf3
alaa <alaa0selim@gmail.com>
2018-08-20 09:01:50 +0300

    clean the code

--- index.js
+++ index.js
@@ -1,57 +1,56 @@
 var exec = require("child_process").execSync;
-var MachineID;
 
-if (process.platform == "darwin") {
-  var res = exec("ioreg -l | grep IOPlatformSerialNumber");
-  var hddid = res.toString().replace(/"/gi, '');
-  hddid = hddid.toString().replace('=', '');
-  hddid = hddid.toString().replace('|', '');
-  hddid = hddid.toString().replace('IOPlatformSerialNumber', '');
-  MachineID =  hddid.trim();
-} else if (process.platform == "win32") {
-   // var res = exec("wmic path win32_physicalmedia get SerialNumber");
-    var res = exec("wmic bios get SerialNumber");
-    var hddid = res.toString().replace("SerialNumber", "");
-    var serial1 = hddid.trim();
+/*
+Get a unique Machine ID on Windows, Mac & Linux 
+By Alaa Selim
+E-mail: alaa0selim@gmail.com
+*/
 
-    var res11 = exec("wmic bios get version");
-    var hddid11 = res11.toString().replace("Version", "");
-    var serial11 = hddid11.trim();
+function GetMachineID(){
 
-    var res2 = exec("wmic baseboard get SerialNumber");
-    var hddid2 = res2.toString().replace("SerialNumber", "");
-    serial2 = hddid2.trim();
+    if (process.platform == "darwin") {
+        var res = exec("ioreg -l | grep IOPlatformSerialNumber");
+        return res.toString().replace(/"/gi, '').replace('=', '').replace('|', '').replace('IOPlatformSerialNumber', '').trim();
+       
+      
+      } else if (process.platform == "win32") {
+         // var res = exec("wmic path win32_physicalmedia get SerialNumber");
+          var res = exec("wmic bios get SerialNumber");
+          var serial0 = res.toString().replace("SerialNumber", "").trim();
+      
+          var bios_version = exec("wmic bios get version");
+          var serial1 = bios_version.toString().replace("Version", "").trim();
+      
+          var SerialNumber = exec("wmic baseboard get SerialNumber");
+          var serial2 = SerialNumber.toString().replace("SerialNumber", "").trim();
+      
+          var baseboard_product = exec("wmic baseboard get Product");
+          var serial3 = baseboard_product.toString().replace("Product", "").trim();
+      
+          var baseboard_Manufacturer = exec("wmic baseboard get Manufacturer");
+          var serial4 = baseboard_Manufacturer.toString().replace("Manufacturer", "").trim();
+      
+          var uuid = exec("wmic path win32_computersystemproduct get uuid");
+          var serial5 = uuid.toString().replace("UUID", "").trim();
+      
+          var IdentifyingNumber = exec("wmic path win32_computersystemproduct get IdentifyingNumber");
+          var serial6 = IdentifyingNumber.toString().replace("IdentifyingNumber", "").trim();
+      
+          var win_Name = exec("wmic path win32_computersystemproduct get Name");
+          var serial7 = win_Name.toString().replace("Name", "").trim();
+      
+          var win_Version = exec("wmic path win32_computersystemproduct get Version");
+          var serial8 = win_Version.toString().replace("Version", "").trim();
+      
+          return serial0 + serial1 +  serial2 +  serial3 + serial4 + serial5 + serial6 + serial7 + serial8;
+      
+      } else if (process.platform == "linux" || process.platform == "freebsd") {
+          var res = exec("/sbin/udevadm info --query=property --name=sda | grep ID_SERIAL_SHORT");
+          return res.toString().replace("ID_SERIAL_SHORT=", "").trim();
+      } else return null;
+      
 
-    var res22 = exec("wmic baseboard get Product");
-    var hddid22 = res22.toString().replace("Product", "");
-    serial22 = hddid22.trim();
+}
 
-    var res23 = exec("wmic baseboard get Manufacturer");
-    var hddid23 = res23.toString().replace("Manufacturer", "");
-    serial23 = hddid23.trim();
 
-    var res3 = exec("wmic path win32_computersystemproduct get uuid");
-    hddid3 = res3.toString().replace("UUID", "");
-    serial3 = hddid3.trim();
-
-    var res33 = exec("wmic path win32_computersystemproduct get IdentifyingNumber");
-    hddid33 = res33.toString().replace("IdentifyingNumber", "");
-    serial33 = hddid33.trim();
-
-    var res34 = exec("wmic path win32_computersystemproduct get Name");
-    hddid34 = res34.toString().replace("Name", "");
-    serial34 = hddid34.trim();
-
-    var res35 = exec("wmic path win32_computersystemproduct get Version");
-    hddid35 = res35.toString().replace("Version", "");
-    serial35 = hddid35.trim();
-
-    MachineID = serial1 + serial11 + serial2 + serial22 + serial23 + serial3 + serial33 + serial34 + serial35;
-
-} else if (process.platform == "linux" || process.platform == "freebsd") {
-    var res = exec("/sbin/udevadm info --query=property --name=sda | grep ID_SERIAL_SHORT");
-    var hddid = res.toString().replace("ID_SERIAL_SHORT=", "");
-    MachineID =  hddid.trim();
-} else MachineID = "";
-
-console.log(MachineID);
+console.log(GetMachineID());
